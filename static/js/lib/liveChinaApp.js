423966aed629e32d1edf556a446ab7d7c74ce09e
sundanping <100001sun>
2017-07-25 11:28:30 +0800

    First change

---
+++ static/js/lib/liveChinaApp.js
@@ -0,0 +1,14 @@
+
+var liveChinaApp=angular.module('liveChinaApp', []);
+var API_URL_ROOT = '';
+liveChinaApp.controller('live', ['$scope', function($scope){
+
+}]);
+
+// liveChinaApp.config(['$routeProvider', function($routeProvider){
+//     $routeProvider
+//         .when('/',{template:'这是首页页面'})
+//         .when('/computers',{template:'这是电脑分类页面'})
+//         .when('/printers',{template:'这是打印机页面'})
+//         .otherwise({redirectTo:'/'});
+// }]);
