327df0ad72fdda44b5e5c2e6d06c2028c3c2a49b
sundanping <454625164@qq.com>
2017-07-25 22:43:08 +0800

    commit

---
+++ src/js/lib/liveChinaApp-route.js
@@ -0,0 +1,13 @@
+
+// liveChinaApp.config(function ($routeProvider, $locationProvider) {
+//     /*
+//      TODO: route rules namespace factory
+//      **/
+//     $routeProvider
+//         .when('/', {
+//             templateUrl: '<a href="">aa</a>',
+//         })
+//         .when('/pc/index', {
+//             templateUrl: '<a href="">aa</a>',
+//             controller: 'HomeProductListController',}
+//         })
---
+++ src/js/lib/liveChinaApp.js
@@ -0,0 +1,109 @@
+
+var liveChinaApp=angular.module('liveChinaApp', []);
+var API_URL_ROOT = 'http://twapi.live.hoge.cn/index.php';
+liveChinaApp.controller('live', ['$scope','$http' ,function($scope,$http){
+    $scope.aa=0;
+    $http({
+        method: 'JSONP',
+        // url: 'http://www.b.com/test.php?callback=JSON_CALLBACK',
+        url:API_URL_ROOT+"?callback=JSON_CALLBACK"+'&m=Apituwenol&c=tuwenol&a=show&custom_appkey=da1c994019b00a760a68e735db9dc281&custom_appid=197',
+        // data:{
+        //     show_tailer:0,
+        //     custom_appkey:'da1c994019b00a760a68e735db9dc281',
+        //     custom_appid:197
+        // },
+    }).success(function (msg) {
+    $scope.liveType=msg;
+        $scope.trailer=[];
+        $scope.history=[];
+        $scope.live=[];
+        // console.log(msg);
+        angular.forEach($scope.liveType,function(data,index){
+            // console.log(index)
+            if(data.time_status===1){
+                $scope.trailer.push(data)
+            }else if(data.time_status===2){
+                $scope.live.push(data)
+
+            }else if(data.time_status===0){
+                $scope.history.push(data)
+
+            }
+        });
+        console.log('$scope.live');
+
+        console.log($scope.live);
+        console.log('$scope.history');
+
+        console.log($scope.history);
+        console.log('$scope.trailer');
+
+        console.log($scope.trailer);
+
+
+    });
+
+
+     $http({
+        method: 'JSONP',
+        // url: 'http://www.b.com/test.php?callback=JSON_CALLBACK',
+        url:API_URL_ROOT+"?callback=JSON_CALLBACK"+'&m=Apituwenol&c=tuwenol&a=detail&custom_appkey=da1c994019b00a760a68e735db9dc281&custom_appid=197',
+        // data:{
+        //     show_tailer:0,
+        //     custom_appkey:'da1c994019b00a760a68e735db9dc281',
+        //     custom_appid:197
+        // },
+    }).success(function (msg) {
+            console.log(msg)
+        });
+       
+
+     $http({
+        method: 'JSONP',
+        // url: 'http://www.b.com/test.php?callback=JSON_CALLBACK',
+        url:API_URL_ROOT+"?callback=JSON_CALLBACK"+'&m=Apituwenol&c=tuwenol&a=detail&custom_appkey=da1c994019b00a760a68e735db9dc281&custom_appid=197',
+        // data:{
+        //     show_tailer:0,
+        //     custom_appkey:'da1c994019b00a760a68e735db9dc281',
+        //     custom_appid:197
+        // },
+    }).success(function (msg) {
+            console.log(msg)
+        });
+       
+     $http({
+        method: 'JSONP',
+        // url: 'http://www.b.com/test.php?callback=JSON_CALLBACK',
+        url:API_URL_ROOT+"?callback=JSON_CALLBACK"+'&m=Apituwenol&c=tuwenol&a=detail&custom_appkey=da1c994019b00a760a68e735db9dc281&custom_appid=197',
+        // data:{
+        //     show_tailer:0,
+        //     custom_appkey:'da1c994019b00a760a68e735db9dc281',
+        //     custom_appid:197
+        // },
+    }).success(function (msg) {
+            console.log(msg)
+        });
+       
+     $http({
+        method: 'JSONP',
+        // url: 'http://www.b.com/test.php?callback=JSON_CALLBACK',
+        url:API_URL_ROOT+"?callback=JSON_CALLBACK"+'&m=Apituwenol&c=tuwenol&a=show_comment&custom_appkey=da1c994019b00a760a68e735db9dc281&custom_appid=197',
+        // data:{
+        //     show_tailer:0,
+        //     custom_appkey:'da1c994019b00a760a68e735db9dc281',
+        //     custom_appid:197
+        // },
+    }).success(function (msg) {
+            console.log(msg)
+        });
+       
+
+}]);
+
+// liveChinaApp.config(['$routeProvider', function($routeProvider){
+//     $routeProvider
+//         .when('/',{template:'这是首页页面'})
+//         .when('/computers',{template:'这是电脑分类页面'})
+//         .when('/printers',{template:'这是打印机页面'})
+//         .otherwise({redirectTo:'/'});
+// }]);
